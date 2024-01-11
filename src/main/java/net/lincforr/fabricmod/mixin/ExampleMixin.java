9a35cbf869dbd8aff7f5da250776df8e499aa843
lincforr <lincforr@gmail.com>
2024-01-11 16:05:01 -0500

    edit: mod id and info

--- src/main/java/net/lincforr/fabricmod/mixin/ExampleMixin.java
+++ src/main/java/net/lincforr/fabricmod/mixin/ExampleMixin.java
@@ -1,6 +1,7 @@
-package net.fabricmc.example.mixin;
+package net.lincforr.fabricmod.mixin;
 
-import net.fabricmc.example.ExampleMod;
+
+import net.lincforr.fabricmod.FabricMod;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -11,6 +12,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class ExampleMixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
-		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
+		FabricMod.LOGGER.info("This line is printed by an example mod mixin!");
 	}
 }
